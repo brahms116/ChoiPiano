ee7b4ad8c6f931f3759d971f410785b3531aa337
David <20544dk@gmail.com>
2020-06-05 20:08:12 +1000

    uggh

--- data.ts
+++ data.ts
@@ -4,4 +4,5 @@ class Data{
     static para2 = `Recipient of the St Peter’s Lutheran College Scholarship, Samuel was the winner in both the St Peter’s Recital Competition and the Concerto Competition for several years. He has also received awards in multiple Brisbane Eisteddfods, the Queensland Piano Competition and won the People’s Choice Award in the 4MBS National Virtuoso of the Year. Samuel has helped raise funds for children in North Korea through a concert and has been a special guest in the Asian Pacific Mayor’s Conference.`
     static para3= `In 2018, Samuel won first prize in the Chopin section of the Queensland Piano Competition, and made his international debut in the South Korean city of Busan. In his first year at Queensland Conservatorium Griffith University, Samuel won the Minnie May Bussey Scholarship. Now in his second year of study, Samuel looks forward to continuing both his studies and performances.`
 }
+
 export default Data
