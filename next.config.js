10028e005f2021a95a77bf9934afeb133285dbc1
David <20544dk@gmail.com>
2020-06-05 18:49:40 +1000

    fixed safari Ios with babel loader for node modules

--- next.config.js
+++ next.config.js
@@ -1,2 +1,18 @@
 const withImages = require('next-images')
-module.exports = withImages()
+
+
+module.exports = withImages({
+    webpack:(config, options)=>{
+        
+        config.module.rules.push(
+            {
+                test: /\.js$/,
+                include: [
+                    /node_modules/
+                ],
+                loader: 'babel-loader'
+            }
+        )
+        return config
+    }
+})
