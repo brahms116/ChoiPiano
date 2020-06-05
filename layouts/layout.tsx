13dd2758fb044b7cec9bf50f8463414ffbb403a8
David <20544dk@gmail.com>
2020-06-06 07:04:57 +1000

    fixed for all platforms

--- layouts/layout.tsx
+++ layouts/layout.tsx
@@ -8,15 +8,27 @@ type layoutProps = {
 }
 
 const Layout = (props:layoutProps)=>{
+
+    
+
+
     return(
-        <div>
+        <motion.div initial={{opacity:0}} 
+            animate={{opacity:1}} 
+            exit={{opacity:0}} 
+            transition={{when:"beforeChildren",staggerChildren:0.3}}
+        >
             <Head>
-                <title>Choi Piano</title>s
+                <title>Choi Piano</title>
+                <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;900&display=swap" rel="stylesheet"></link>
+                {/* <style>
+                    @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;900&display=swap');
+                </style> */}
             </Head>
             <Navigation/>
 
             <main>{props.children}</main>
-        </div>
+        </motion.div>
     )
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import {AppProps} from 'next/app' 
 import '../styles/styles.css'
-
+import {AnimatePresence} from 'framer-motion'
 
 const App= ({Component, pageProps, router}:AppProps)=>{
     return (
+        <AnimatePresence exitBeforeEnter>
             <Component {...pageProps} key={router.route }/>
+        </AnimatePresence>
     )
 
 }
