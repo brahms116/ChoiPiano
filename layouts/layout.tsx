6c3a09a78553600a48eaae5a320c185335c85c78
David <20544dk@gmail.com>
2020-06-08 14:52:03 +1000

    google analytics

--- layouts/layout.tsx
+++ layouts/layout.tsx
@@ -45,15 +45,7 @@ const Layout = (props:layoutProps)=>{
                 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168770266-1"></script>
                 <script>
                     dangerouslySetInnerHTML={{
-                        __html: `<!-- Global site tag (gtag.js) - Google Analytics -->
-                        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168770266-1"></script>
-                        <script>
-                          window.dataLayer = window.dataLayer || [];
-                          function gtag(){dataLayer.push(arguments);}
-                          gtag('js', new Date());
-                        
-                          gtag('config', 'UA-168770266-1');
-                        </script>                        
+                        __html: `<!-- Global site tag (gtag.js) - Google Analytics -->              
                         `,
                     }}
                 </script>
